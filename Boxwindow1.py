de4df3600ed51a5348ca49d9bf7b3f80e7e5ff1a
aloileseigneur <109355554+aloileseigneur@users.noreply.github.com>
2022-07-19 10:40:28 +0200

    Update Boxwindow1.py

--- Boxwindow1.py
+++ Boxwindow1.py
@@ -1,4 +1,5 @@
 
+from concurrent.futures import thread
 from math import radians
 from random import randint
 from pyqtgraph.Qt import QtGui, QtCore, QtWidgets
@@ -31,16 +32,16 @@ class Boxmainwindow(Ui_MainWindow):
 
     def __init__(self,window):
         self.setupUi(window)
-
-        self.pushButton.clicked.connect(self.window)
+        self.window()
+        # self.pushButton.clicked.connect(self.window)
     
     def window(self):
-        self.window1 = pg.GraphicsWindow(title="SEMELLES", size = (1920,1080)) # creates a window
+        #self.window1 = pg.GraphicsWindow(title="SEMELLES", size = (1920,1080)) # creates a window
         
-        p = self.window1.addPlot(title="Semelles Gauche")
-        p.setYRange(0,1000)
-        p.setLabel(axis='left', text='Pression(Pa)')
-        p.setLabel(axis='bottom', text='Temps')
+        self.p = self.graphicsView.addPlot(title="Semelles Gauche")
+        self.p.setYRange(0,1000)
+        self.p.setLabel(axis='left', text='Pression(Pa)')
+        self.p.setLabel(axis='bottom', text='Temps')
         color_blue = QtGui.QColor(0, 0, 255)
         color_aqua = QtGui.QColor(0, 255, 255)
         color_red = QtGui.QColor(255, 0, 0)
@@ -51,16 +52,16 @@ class Boxmainwindow(Ui_MainWindow):
         pen_aqua = pg.mkPen(color_aqua, width=3)
         pen_black = pg.mkPen(color_black, width=3)
         pen_green = pg.mkPen(color_green, width=3)
-        self.curveTi = p.plot(pen = pen_blue)                        # create an empty "plot" (a curve to plot)
-        self.curveTe = p.plot(pen = pen_red)
-        self.curveMi = p.plot(pen = pen_aqua)                        # create an empty "plot" (a curve to plot)
-        self.curveMe = p.plot(pen = pen_black)
-        self.curveDt = p.plot(pen = pen_green)
+        self.curveTi = self.p.plot(pen = pen_blue)                        # create an empty "plot" (a curve to plot)
+        self.curveTe = self.p.plot(pen = pen_red)
+        self.curveMi = self.p.plot(pen = pen_aqua)                        # create an empty "plot" (a curve to plot)
+        self.curveMe = self.p.plot(pen = pen_black)
+        self.curveDt = self.p.plot(pen = pen_green)
 
-        p1 = self.window1.addPlot(title="Semelles Droite")
-        p1.setYRange(0,1000)
-        p1.setLabel(axis='left', text='Pression(Pa)')
-        p1.setLabel(axis='bottom', text='Temps')
+        self.p1 = self.graphicsView.addPlot(title="Semelles Droite")
+        self.p1.setYRange(0,1000)
+        self.p1.setLabel(axis='left', text='Pression(Pa)')
+        self.p1.setLabel(axis='bottom', text='Temps')
         color_blue1 = QtGui.QColor(0, 0, 255)
         color_aqua1 = QtGui.QColor(0, 255, 255)
         color_red1 = QtGui.QColor(255, 0, 0)
@@ -71,14 +72,17 @@ class Boxmainwindow(Ui_MainWindow):
         pen_aqua1 = pg.mkPen(color_aqua1, width=3)
         pen_black1 = pg.mkPen(color_black1, width=3)
         pen_green1 = pg.mkPen(color_green1, width=3)
-        self.curveTiD = p1.plot(pen = pen_blue1)                        # create an empty "plot" (a curve to plot)
-        self.curveTeD = p1.plot(pen = pen_red1)
-        self.curveMiD = p1.plot(pen = pen_aqua1)                        # create an empty "plot" (a curve to plot)
-        self.curveMeD = p1.plot(pen = pen_black1)
-        self.curveDtD = p1.plot(pen = pen_green1)
+        self.curveTiD = self.p1.plot(pen = pen_blue1)                        # create an empty "plot" (a curve to plot)
+        self.curveTeD = self.p1.plot(pen = pen_red1)
+        self.curveMiD = self.p1.plot(pen = pen_aqua1)                        # create an empty "plot" (a curve to plot)
+        self.curveMeD = self.p1.plot(pen = pen_black1)
+        self.curveDtD = self.p1.plot(pen = pen_green1)
+
+        self.serial = Serial(self)
+        self.update = Update(self)
     
         
-class serial(QThread):
+class Serial(QThread):
     windowWidth = 500                    # width of the window displaying the curve
     Ti = linspace(0,0,windowWidth)          # create array that will contain the relevant time series     
     Te = linspace(0,0,windowWidth)
@@ -94,7 +98,8 @@ class serial(QThread):
     ptr = 0  
     ptr1 = 0 
     def __init__(self,parent=None):
-        super(serial,self).__init__(parent)
+        super(Serial,self).__init__(parent)
+        self._parent = parent
 
     def setgui(self, gui):
         self.gui = gui
@@ -151,17 +156,17 @@ class serial(QThread):
                 self.Dt1[-1] = float(value4)  
 
             self.ptr1 += 1                              # update x position for displaying the curve
-            self.curveTiD.setData(self.Ti1)     
-            self.curveTeD.setData(self.Te1)
-            self.curveMiD.setData(self.Mi1)     
-            self.curveMeD.setData(self.Me1)
-            self.curveDtD.setData(self.Dt1)       
+            self._parent.curveTiD.setData(self.Ti1)     
+            self._parent.curveTeD.setData(self.Te1)
+            self._parent.curveMiD.setData(self.Mi1)     
+            self._parent.curveMeD.setData(self.Me1)
+            self._parent.curveDtD.setData(self.Dt1)       
                             
-            self.curveTiD.setPos(self.ptr1,0)                   # set x position in the graph to 0
-            self.curveTeD.setPos(self.ptr1,0)
-            self.curveMiD.setPos(self.ptr1,0)                   # set x position in the graph to 0
-            self.curveMeD.setPos(self.ptr1,0)
-            self.curveDtD.setPos(self.ptr1,0)
+            self._parent.curveTiD.setPos(self.ptr1,0)                   # set x position in the graph to 0
+            self._parent.curveTeD.setPos(self.ptr1,0)
+            self._parent.curveMiD.setPos(self.ptr1,0)                   # set x position in the graph to 0
+            self._parent.curveMeD.setPos(self.ptr1,0)
+            self._parent.curveDtD.setPos(self.ptr1,0)
                                 # create an empty "plot" (a curve to plot)
             ##gauche
 
@@ -212,19 +217,19 @@ class serial(QThread):
                 self.Dt[-1] = float(valueg4)
 
             self.ptr += 1                              # update x position for displaying the curve
-            self.curveTi.setData(self.Ti)     
-            self.curveTe.setData(self.Te)
-            self.curveMi.setData(self.Mi)     
-            self.curveMe.setData(self.Me)
-            self.curveDt.setData(self.Dt)
+            self._parent.curveTi.setData(self.Ti)     
+            self._parent.curveTe.setData(self.Te)
+            self._parent.curveMi.setData(self.Mi)     
+            self._parent.curveMe.setData(self.Me)
+            self._parent.curveDt.setData(self.Dt)
                                                               # set the curve with this data
-            self.curveTi.setPos(self.ptr,0)                   # set x position in the graph to 0
-            self.curveTe.setPos(self.ptr,0)
-            self.curveMi.setPos(self.ptr,0)                   # set x position in the graph to 0
-            self.curveMe.setPos(self.ptr,0)
-            self.curveDt.setPos(self.ptr,0)
+            self._parent.curveTi.setPos(self.ptr,0)                   # set x position in the graph to 0
+            self._parent.curveTe.setPos(self.ptr,0)
+            self._parent.curveMi.setPos(self.ptr,0)                   # set x position in the graph to 0
+            self._parent.curveMe.setPos(self.ptr,0)
+            self._parent.curveDt.setPos(self.ptr,0)
 
-class update(QThread):
+class Update(QThread):
 
     
     windowWidth = 500                    # width of the window displaying the curve
@@ -242,12 +247,14 @@ class update(QThread):
     ptr1 = 0  
 
     def __init__(self,parent=None):
-        super(update,self).__init__(parent)
+        super(Update,self).__init__(parent)
+        self._parent = parent
 
     def setgui(self, gui):
         self.gui = gui
 
     def run1(self):
+        
         while True:
             self.Ti[:-1] = self.Ti[1:]  
             self.Te[:-1] = self.Te[1:]
@@ -272,11 +279,19 @@ class update(QThread):
 app = QtWidgets.QApplication([])
 MainWindow = QtWidgets.QMainWindow()
 
-widget = serial()
-widget2 = update()
-widget.start()
-widget2.start()
+Serial().start()
+Update().start()
+
 
 ui = Boxmainwindow(MainWindow)
 MainWindow.show()
 app.exec()
+Serial().terminate()
+Update().terminate()
+Serial().wait()
+Update().wait()
+
+##
+# join thread
+# les stop aussi
+# ##
