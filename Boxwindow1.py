680c9ffa251929e1b4aaa0d7ad741537a718a51f
aloileseigneur <109355554+aloileseigneur@users.noreply.github.com>
2022-08-01 15:22:26 +0200

    Update Boxwindow1.py

--- Boxwindow1.py
+++ Boxwindow1.py
@@ -1,24 +1,28 @@
 from random import randint
 from pyqtgraph.Qt import QtGui,  QtWidgets
 import pyqtgraph as pg
+import pyqtgraph.exporters
 import serial
 from MainWindow import *
+from secondwindow import *
 from numpy import linspace
 from PyQt5.QtCore import QThread
 
 ser = serial.Serial(port = "/dev/ttyUSB0",baudrate=230400,timeout=1) # configuration du port série
 
-class Window(Ui_MainWindow):
+class Window(Ui_MainWindow, Ui_Form):
     pg.setConfigOption('background','w')
     pg.setConfigOption('foreground', 'k')
 
      
 
-    def __init__(self,window):
+    def __init__(self,window, secondwindow):
         super().__init__()
         self.setupUi(window)
         
-        self.windowWidth = 500                    # width of the window displaying the curve
+        
+
+        self.windowWidth = 600                  # width of the window displaying the curve
         self.Ti = linspace(0,0,self.windowWidth)          # create array that will contain the relevant time series     
         self.Te = linspace(0,0,self.windowWidth)
         self.Mi = linspace(0,0,self.windowWidth)          # create array that will contain the relevant time series     
@@ -33,8 +37,10 @@ class Window(Ui_MainWindow):
         self.ptr = 0  
         self.ptr1 = 0
         
+        
+
         self.p = self.graphicsView.addPlot(title="Semelles Gauche")
-        self.p.setYRange(0,1000)
+        self.p.setYRange(0, 1000, padding=0, update=True)
         self.p.setLabel(axis='left', text='Pression(Pa)')
         self.p.setLabel(axis='bottom', text='Temps')
         color_blue = QtGui.QColor(0, 0, 255)
@@ -53,8 +59,8 @@ class Window(Ui_MainWindow):
         self.curveMe = self.p.plot(pen = pen_black)
         self.curveDt = self.p.plot(pen = pen_green)
 
-        self.p1 = self.graphicsView.addPlot(title="Semelles Droite")
-        self.p1.setYRange(0,1000)
+        self.p1 = self.graphicsView.addPlot(title="Semelles Gauche")
+        self.p1.setYRange(0, 1000, padding=0, update=True)
         self.p1.setLabel(axis='left', text='Pression(Pa)')
         self.p1.setLabel(axis='bottom', text='Temps')
         color_blue1 = QtGui.QColor(0, 0, 255)
@@ -69,29 +75,64 @@ class Window(Ui_MainWindow):
         pen_green1 = pg.mkPen(color_green1, width=3)
         self.curveTiD = self.p1.plot(pen = pen_blue1)                        # create an empty "plot" (a curve to plot)
         self.curveTeD = self.p1.plot(pen = pen_red1)
-        self.curveMiD = self.p1.plot(pen = pen_aqua1)                        # create an empty "plot" (a curve to plot)
-        self.curveMeD = self.p1.plot(pen = pen_black1)
+        self.curveMiD = self.p1.plot(pen = pen_black1)                        # create an empty "plot" (a curve to plot)
+        self.curveMeD = self.p1.plot(pen = pen_aqua1)
         self.curveDtD = self.p1.plot(pen = pen_green1)
+        
+        self.pushButton.clicked.connect(lambda:self.montrer())
+        self.pushButton_2.clicked.connect(lambda:self.cacher())
+        self.pushButton_4.clicked.connect(lambda:self.enregistrer())
 
+        self.actionAvanc.triggered.connect(lambda:self.opensecond())
+        
+
+        self.vert_1.show()
+        self.vert_2.show()
+        self.vert_3.show()
+        self.vert_4.show()
+        self.vert_5.show()
+        self.rouge_1.hide()
+        self.rouge_2.hide()
+        self.rouge_3.hide()
+        self.rouge_4.hide()
+        self.rouge_5.hide()
+        
+
+        
 
         self.serial = Serial(self)
         self.serial.start()
-        # self.update = Update(self)
-        #self.update.start()
 
     def __del__(self):
-        #self.update.terminate()
         self.serial.terminate()
-        # self.update.wait()
         self.serial.wait()
+
+    def montrer(self):
+        self.pushButton_3.hide()
+            
+    def cacher(self):
+        self.pushButton_3.show()
     
+    def opensecond(self):
+            self.secondwindow = QtWidgets.QMainWindow()
+            self.ui2 = Ui_Form()
+            self.ui2.setupUi(self.secondwindow)
+            self.secondwindow.show()
         
-class Serial(QThread):
+    def enregistrer(self):
+        self.exporter = pg.exporters.ImageExporter() #Juste avant les exportateurs pg.QtGui.QApplication.processEvents()Appel!
+        self.exporter.parameters()['width'] = 100 
+        self.exporter.export("test.png ")
+        print("exported")
+
+
+class Serial(QThread,Ui_Form):
     def __init__(self,parent):
         super(Serial,self).__init__()
         self._parent = parent
+        
 
-        self.windowWidth = 500                    # width of the window displaying the curve
+        self.windowWidth = 600                    # width of the window displaying the curve
         self.Ti = linspace(0,0,self.windowWidth)          # create array that will contain the relevant time series     
         self.Te = linspace(0,0,self.windowWidth)
         self.Mi = linspace(0,0,self.windowWidth)          # create array that will contain the relevant time series     
@@ -105,6 +146,7 @@ class Serial(QThread):
         
         self.ptr = 0  
         self.ptr1 = 0 
+        self.cpt=0
 
     def setgui(self, gui):
         self.gui = gui
@@ -113,20 +155,7 @@ class Serial(QThread):
     def run(self):
         while True:
 
-            # ################test valeurs###################
-            # int_taloni = randint(0,100)            
-            # int_talone = randint(0,100)
-            # int_millieui = randint(0,100)             
-            # int_millieue = randint(0,100)
-            # int_doigt = randint(0,100) 
-
-
-            # value = int_taloni              
-            # value1 = int_talone
-            # value2 = int_millieui               
-            # value3 = int_millieue
-            # value4 = int_doigt
-            # ###############################################
+            
             self.Ti[:-1] = self.Ti[1:]  
             self.Te[:-1] = self.Te[1:]
             self.Mi[:-1] = self.Mi[1:]
@@ -137,6 +166,20 @@ class Serial(QThread):
             self.Mi1[:-1] = self.Mi1[1:]
             self.Me1[:-1] = self.Me1[1:]
             self.Dt1[:-1] = self.Dt1[1:]  
+
+            # ################test valeurs###################
+            # int_taloni = 500      
+            # int_talone = 400
+            # int_millieui = 300             
+            # int_millieue = 200
+            # int_doigt = 100
+
+            # value = int_taloni              
+            # value1 = int_talone
+            # value2 = int_millieui               
+            # value3 = int_millieue
+            # value4 = int_doigt
+            # ###############################################   
             
             receive = ser.read(22)  #reception de 22 bytes 
             receivehex = receive.hex() # conversion en héxadecimal
@@ -156,7 +199,7 @@ class Serial(QThread):
                 print(liste[16:18])
                 value = int_taloni               # read line (single value) from the serial port
                 value1 = int_talone
-                value2 = int_millieui               # read line (single value) from the serial port
+                value2 = int_millieui            # read line (single value) from the serial port
                 value3 = int_millieue
                 value4 = int_doigt              # read line (single value) from the serial port
                 
@@ -165,7 +208,44 @@ class Serial(QThread):
                 self.Te1[-1] = float(value1)
                 self.Mi1[-1] = float(value2)                 # vector containing the instantaneous values      
                 self.Me1[-1] = float(value3)
-                self.Dt1[-1] = float(value4)  
+                self.Dt1[-1] = float(value4) 
+
+                if int_doigt >= 200  :
+                    self._parent.rouge_11.show()
+                    self._parent.vert_11.hide()
+                else:
+                    self._parent.rouge_11.hide()
+                    self._parent.vert_11.show()
+
+                if int_millieui >= 200  :
+                    self._parent.rouge_13.show()
+                    self._parent.vert_13.hide()
+                else:
+                    self._parent.rouge_13.hide()
+                    self._parent.vert_13.show()
+
+                if int_millieue >= 200 :
+                    self._parent.rouge_12.show()
+                    self._parent.vert_12.hide()
+                else:
+                    self._parent.rouge_12.hide()
+                    self._parent.vert_12.show()
+
+                if int_taloni >= 200 :
+                    self._parent.rouge_14.show()
+                    self._parent.vert_14.hide()
+                else:
+                    self._parent.rouge_14.hide()
+                    self._parent.vert_14.show()
+
+                if int_talone >= 200 :
+                    self._parent.rouge_15.show()
+                    self._parent.vert_15.hide()
+                else:
+                    self._parent.rouge_15.hide()
+                    self._parent.vert_15.show()
+
+      
 
             self.ptr1 += 1                              # update x position for displaying the curve
             self._parent.curveTiD.setData(self.Ti1)     
@@ -182,16 +262,14 @@ class Serial(QThread):
                                 # create an empty "plot" (a curve to plot)
             ##gauche
 
-            
 
-            
 
             # ###############test valeurs###################
-            # int_taloni = randint(0,100)            
-            # int_talone = randint(0,100)
-            # int_millieui = randint(0,100)             
-            # int_millieue = randint(0,100)
-            # int_doigt = randint(0,100) 
+            # int_taloni = 100          
+            # int_talone = 200
+            # int_millieui = 300             
+            # int_millieue = 400
+            # int_doigt = 500
 
 
             # valueg = int_taloni              
@@ -203,20 +281,19 @@ class Serial(QThread):
 
 
             if liste[2:3]=='0':
-                print(liste)
                 int_talonig = int(liste[6:9], 16)  # shift data in the temporal mean 1 sample left
-                print(liste[6:9])                  
+                print(liste[6:9])                                     
                 int_taloneg = int(liste[9:11], 16)# shift data in the temporal mean 1 sample left
                 print(liste[9:11])
-                int_millieuig = int(liste[11:14], 16) # shift data in the temporal mean 1 sample left
-                print(liste[11:14])                   
-                int_millieueg = int(liste[14:16], 16)# shift data in the temporal mean 1 sample left
-                print(liste[14:16])
+                int_millieuig = int(liste[14:16], 16) # shift data in the temporal mean 1 sample left
+                print(liste[14:16])                   
+                int_millieueg = int(liste[11:14], 16)# shift data in the temporal mean 1 sample left
+                print(liste[11:14])
                 int_doigtg = int(liste[16:18], 16) # shift data in the temporal mean 1 sample left
                 print(liste[16:18])
                 valueg = int_talonig               # read line (single value) from the serial port
                 valueg1 = int_taloneg
-                valueg2 = int_millieuig              # read line (single value) from the serial port
+                valueg2 = int_millieuig            # read line (single value) from the serial port
                 valueg3 = int_millieueg
                 valueg4 = int_doigtg 
 
@@ -225,6 +302,41 @@ class Serial(QThread):
                 self.Mi[-1] = float(valueg2)                 # vector containing the instantaneous values      
                 self.Me[-1] = float(valueg3)
                 self.Dt[-1] = float(valueg4)
+
+                if int_doigtg >= 200  :
+                    self._parent.rouge_1.show()
+                    self._parent.vert_1.hide()
+                else:
+                    self._parent.rouge_1.hide()
+                    self._parent.vert_1.show()
+
+                if int_millieuig >= 200  :
+                    self._parent.rouge_2.show()
+                    self._parent.vert_2.hide()
+                else:
+                    self._parent.rouge_2.hide()
+                    self._parent.vert_2.show()
+
+                if int_millieueg >= 200 :
+                    self._parent.rouge_3.show()
+                    self._parent.vert_3.hide()
+                else:
+                    self._parent.rouge_3.hide()
+                    self._parent.vert_3.show()
+
+                if int_talonig >= 200 :
+                    self._parent.rouge_4.show()
+                    self._parent.vert_4.hide()
+                else:
+                    self._parent.rouge_4.hide()
+                    self._parent.vert_4.show()
+
+                if int_taloneg >= 200 :
+                    self._parent.rouge_5.show()
+                    self._parent.vert_5.hide()
+                else:
+                    self._parent.rouge_5.hide()
+                    self._parent.vert_5.show()
 
             self.ptr += 1                              # update x position for displaying the curve
             self._parent.curveTi.setData(self.Ti)     
@@ -238,12 +350,15 @@ class Serial(QThread):
             self._parent.curveMi.setPos(self.ptr,0)                   # set x position in the graph to 0
             self._parent.curveMe.setPos(self.ptr,0)
             self._parent.curveDt.setPos(self.ptr,0)
-
+    
 
 
 app = QtWidgets.QApplication([])
 MainWindow = QtWidgets.QMainWindow()
+secondwindow = QtWidgets.QWidget()
 
-ui = Window(MainWindow)
+ui = Window(MainWindow, secondwindow)
 MainWindow.show()
 app.exec()
+
+
